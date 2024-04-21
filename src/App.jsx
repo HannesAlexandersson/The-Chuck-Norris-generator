f91eac21ffa7f7d6d5a49f4f88cdcc4a0bd21f6f
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-21 14:33:33 +0200

    fetch the API on initial page load

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { useState, useEffect } from 'react';
-import { Header } from './components/index.js';
-import { getJoke } from './apiFunctions/index.js';//get all our API functions imported
-import './App.css';
+import { useState, useEffect } from "react";
+import { Header } from "./components/index.js";
+import { getJoke } from "./apiFunctions/index.js"; //get all our API functions imported
+import "./App.css";
 
 function App() {
-  const [joke, setJoke] = useState('');//we store the joke in a state
-  const [selectedCategory, setSelectedCategory] = useState('');//the categories are also stored in a state, this state updates when user choose a category
-  const [loaded, setLoaded] = useState(false);//a state to handle if the " " should render
-
+  const [joke, setJoke] = useState(""); //we store the joke in a state
+  const [selectedCategory, setSelectedCategory] = useState(""); //the categories are also stored in a state, this state updates when user choose a category
+  const [loaded, setLoaded] = useState(false); //a state to handle if the " " should render
 
   //the clickhandler uses the imported fetch function to actually get the random joke from the API
   const handleGetJoke = async () => {
     try {
-      const data = await getJoke('random');
+      const data = await getJoke("random");
       setJoke(data.value);
-      setLoaded(true);//set the loaded state to display the "" + the actual joke      
+      setLoaded(true); //set the loaded state to display the "" + the actual joke
     } catch (error) {
-      console.error('naah... Chuck Norris stole the joke again:', error);
+      console.error("naah... Chuck Norris stole the joke again:", error);
     }
   };
 
-  
+  // fetch on initial page load
+  useEffect(() => {
+    handleGetJoke();
+  }, []);
+
   useEffect(() => {
     const fetchData = async () => {
       if (selectedCategory) {
@@ -29,23 +32,22 @@ function App() {
           setJoke(data.value);
           setLoaded(true);
         } catch (error) {
-          console.error('naah... Chuck Norris stole the joke again:', error);
+          console.error("naah... Chuck Norris stole the joke again:", error);
         }
       }
     };
-  
-    fetchData(); 
-  }, [selectedCategory]);//we hook renders evertime the user selects an category
-    
-  
- 
+
+    fetchData();
+  }, [selectedCategory]); //we hook renders evertime the user selects an category
+
   //the category handler updates the category state when the user chooses a category
   const handleCategorySelect = async (category) => {
-    setSelectedCategory(category);   
+    setSelectedCategory(category);
   };
 
   //console.log(selectedCategory, 'app category'); /*DEBUG */
   return (
+    <>
       <Header
         handleGetJoke={handleGetJoke}
         handleCategorySelect={handleCategorySelect}
@@ -53,61 +55,17 @@ function App() {
         setSelectedCategory={setSelectedCategory}
       />
 
-        <div className="displayContainer">
-
-          {loaded ? (
-            <><span>&quot;</span><p>{joke}</p><span>&quot;</span></>
-          ) : (
-            <></>
-          )}
-            
-        </div>
+      <div className="displayContainer">
+        {loaded ? (
+          <>
+            <p>"{joke}"</p>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 }
 
 export default App;
-
-/*
- <div className='page-wrapper'>
-      <div className='main-container'>
-        <div className='headerWrap'>
-          <h1>Get your daily Chuck Norris joke</h1>
-        </div>   
-
-        <div className='randomJoke-container'>     
-          <button onClick={handleGetJoke}>Get a random joke</button>
-
-          <div className="displayBox">
-            <p>{joke}</p>
-          </div>
-          
-
-          </div>
-        </div>
-
-        <div className='secondary-container'>
-
-          <div className='categories-container'>  
-
-            <select value={selectedCategory} onChange={handleCategoryChange}>
-              <option value="">Select a category</option>
-              {categoriesData.map((category, index) => (
-                <option key={index} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-            <button onClick={handleGetCategoryJoke}>Get joke by category</button>
-
-            <div className="displayBox">
-              <p>{catJoke}</p>
-            </div>
-
-          </div>
-
-        </div>
-
-
-      </div>
-*/
