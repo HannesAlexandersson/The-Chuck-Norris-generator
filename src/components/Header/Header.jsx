6d86fe170146596b89cf3b752c3286572816d2e4
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-23 14:08:33 +0200

    add title with infinite x-axis scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,36 @@ import { CategoryBtn, Jokebtn } from '../index.js';
 import style from './header.module.css';
 
 function Header({ selectedCategory, setSelectedCategory, handleGetJoke }) {
+  const count = 30;
+
   return (
     <>
-      <header className={style.container}>
-        <Jokebtn handleGetJoke={handleGetJoke} />
-
-        <CategoryBtn
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
+      <header>
+        <div className={style.titleBox}>
+          <div className={style.titleWrapper}>
+            <h1>***</h1>
+            <h1>The Chuck Norris Generator</h1>
+            <h1>***</h1>
+            <h1>The Chuck Norris Generator</h1>
+            <h1>***</h1>
+            <h1>The Chuck Norris Generator</h1>
+          </div>
+          <div className={style.titleWrapper} aria-hidden="true">
+            <h1>***</h1>
+            <h1>The Chuck Norris Generator</h1>
+            <h1>***</h1>
+            <h1>The Chuck Norris Generator</h1>
+            <h1>***</h1>
+            <h1>The Chuck Norris Generator</h1>
+          </div>
+        </div>
+        <div className={style.buttons}>
+          <Jokebtn handleGetJoke={handleGetJoke} />
+          <CategoryBtn
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        </div>
       </header>
     </>
   );
