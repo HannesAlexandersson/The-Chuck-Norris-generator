8748387c3e872145718ed3ea068e64b0ed70e81c
Hannes Alexandersson <S103154@gmail.com>
2024-04-23 16:25:36 +0200

    final push

--- src/components/Speech/Speech.jsx
+++ src/components/Speech/Speech.jsx
@@ -1,28 +1,53 @@
 import React, { useState, useEffect } from "react";
+import { CircleStop, CirclePause, CirclePlay } from 'lucide-react';
+import style from './speech.module.css';
 
 const TextToSpeech = ({ joke }) => {
   const [isPaused, setIsPaused] = useState(false);
   const [utterance, setUtterance] = useState(null);
+  const [voice, setVoice] = useState(null);
+  const [rate, setRate] = useState(1);
 
   useEffect(() => {
+    
     const synth = window.speechSynthesis;
     const u = new SpeechSynthesisUtterance(joke);
+    
+
+    const voices = window.speechSynthesis.getVoices();
+
+    const englishVoices = voices.filter(voice => {
+      return voice.lang == 'en-US';
+    });
+  
+    setVoice(englishVoices[0]);
+    console.log(englishVoices);
+
 
     setUtterance(u);
+    setRate(0.5);
+    
 
     return () => {
       synth.cancel();
     };
   }, [joke]);
+  
+  
 
   const handlePlay = () => {
     const synth = window.speechSynthesis;
+    synth.lang = "en-US";
 
     if (isPaused) {
       synth.resume();
+    }else{
+        utterance.voice = voice;
+        utterance.rate = rate;
+        synth.speak(utterance);
     }
 
-    synth.speak(utterance);
+    
 
     setIsPaused(false);
   };
@@ -44,10 +69,10 @@ const TextToSpeech = ({ joke }) => {
   };
 
   return (
-    <div>
-      <button onClick={handlePlay}>{isPaused ? "Resume" : "Play"}</button>
-      <button onClick={handlePause}>Pause</button>
-      <button onClick={handleStop}>Stop</button>
+    <div className={style.btns}>
+      <CirclePlay className={style.start} onClick={handlePlay}>{isPaused ? "Resume" : "Play"}</CirclePlay>
+      <CirclePause className={style.start} onClick={handlePause}>Pause</CirclePause>
+      <CircleStop className={style.start} onClick={handleStop}>Stop</CircleStop>
     </div>
   );
 };
