22f66016a8f83b9974d0ff7e94fd1c0bedc50ff1
Hannes Alexandersson <S103154@gmail.com>
2024-04-23 13:59:13 +0200

    Update EmailPopUp.jsx

--- src/components/EmailPopUp/EmailPopUp.jsx
+++ src/components/EmailPopUp/EmailPopUp.jsx
@@ -4,20 +4,17 @@ import styles from './popup.module.css';
 
 function EmailPopUp({ isOpen, onClose, handleGetTxtFile  }) {
     
-  
-    
-  
     return (
         <div className={`${styles.popup} ${isOpen ? styles.open : ''}`}>
-        <div className={styles.popupContent}>
-          <div className={styles.closeContainer}>
-            <span className={styles.close} onClick={onClose}>&times;</span>
+          <div className={styles.popupContent}>
+            <div className={styles.closeContainer}>
+              <span className={styles.close} onClick={onClose}>&times;</span>
+            </div>
+            <h2>Save the jokes</h2>
+            <p>If you want to download the saved jokes as a textfile, simply press the red button!</p>          
+            <button className={styles.saveBtn} onClick={handleGetTxtFile}>Save</button>
           </div>
-          <h2>Save the jokes</h2>
-          <p>If you want to download the saved jokes as a textfile, simply press the red button!</p>          
-          <button className={styles.saveBtn} onClick={handleGetTxtFile}>Save</button>
         </div>
-      </div>
     
     );
   }
